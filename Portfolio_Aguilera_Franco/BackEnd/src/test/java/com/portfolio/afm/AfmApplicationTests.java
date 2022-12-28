f41230fbf23f33c05622910a0493b9f38ef0e04e
Aguilera Franco <francottm@gmail.com>
2022-12-28 13:19:12 -0300

    se crea carpeta frontend y backend

---
+++ Portfolio_Aguilera_Franco/BackEnd/src/test/java/com/portfolio/afm/AfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.afm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
